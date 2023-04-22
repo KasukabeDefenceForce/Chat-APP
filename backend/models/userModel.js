92967115835084a801a7be41367afaf624e625c3
AbhinavOhri <abhinavohri13@gmail.com>
2023-04-22 20:04:15 +0530

    Created schemas for chat, message and user.

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
